9557e50d74f1cdde1dcf50eb0f29ae23874b9312
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-06-28 20:11:49 +0700

    add-box-choose-or-create-room

--- src/components/CreateRoom.vue
+++ src/components/CreateRoom.vue
@@ -2,14 +2,32 @@
   <div>
     <div id="containers" class="container">
 
-      <h1>oiiii</h1>
-      <div class="row center">
-        <div id="chooseroom" class="col s6 center">
-          <h1>oiii</h1>
-        </div>
-        <div id="createroom" class="col s6 center">
-          <h1>oiii</h1>
-        </div>
+      <h1>Need For Click</h1>
+      <div id="rowroom" class="row">
+        <div class="col s1"></div>
+
+        <a href="">
+          <div id="createroom" class="col s5 center">
+            <h1>Create Room</h1>
+            <div class="overlay">
+              <div class="text">
+                <h2>Let's Go...</h2>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="">
+          <div id="chooseroom" class="col s5 center">
+            <h1>Choose Room</h1>
+            <div class="overlay">
+              <div class="text">
+                <h2>Let's Go...</h2>
+              </div>
+            </div>
+          </div>
+        </a>
+
       </div>
 
     </div>
@@ -23,17 +41,67 @@ export default {}
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 #containers {
-  border: 1px solid black;
+  /* border: 1px solid black; */
 }
 
 #createroom {
-  border: 1px solid black;
-    /* margin-left: 5px; */
-
+  border: 2px solid beige;
+  margin-right: 20px;
+  /* background-color: rgb(149, 229, 243); */
+  height: 300px;
+  border-radius: 10px;
+  position: relative;
 }
+
 #chooseroom {
-  border: 1px solid black;
-  /* margin-right: 5px; */
-  
+  border: 2px solid beige;
+  margin-right: 10px;
+  /* background-color: rgb(149, 229, 243); */
+  height: 300px;
+  border-radius: 10px;
+  position: relative;
+}
+
+a:hover {
+  color: aqua;
+}
+
+/* ======== */
+.overlay {
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  right: 0;
+  background-color: #c4c2c0;
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  transition: 0.5s ease;
+  border-radius: 10px;
+}
+
+#createroom:hover .overlay {
+  bottom: 0;
+  height: 100%;
+}
+
+#chooseroom:hover .overlay {
+  bottom: 0;
+  height: 100%;
+}
+
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+a:visited {
+  color: rgb(169, 170, 172);
 }
 </style>
