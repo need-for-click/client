f8f1dec21f5dc3861ee04875d8d62376cb5c6ead
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-06-28 19:00:25 +0700

    new-file-create-room

---
+++ src/components/CreateRoom.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="containers" class="container">
+
+      <h1>oiiii</h1>
+      <div class="row center">
+        <div id="chooseroom" class="col s6 center">
+          <h1>oiii</h1>
+        </div>
+        <div id="createroom" class="col s6 center">
+          <h1>oiii</h1>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+#containers {
+  border: 1px solid black;
+}
+
+#createroom {
+  border: 1px solid black;
+    /* margin-left: 5px; */
+
+}
+#chooseroom {
+  border: 1px solid black;
+  /* margin-right: 5px; */
+  
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
+    <!-- <img src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- ======== di hapus ========= -->
+    <CreateRoom/>
+        <!-- ======== di hapus ========= -->
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+    // <!-- ======== di hapus ========= -->
+
+import CreateRoom from '@/components/CreateRoom.vue'
+    // <!-- ======== di hapus ========= -->
+
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld,
+//========== di hapus =============
+    CreateRoom
+    //========== di hapus =============
+
   }
 }
 </script>
