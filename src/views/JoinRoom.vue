bc436967d606b9c7d8f8add211d7c1001c9d3acc
Dwiky Erlangga <dwikyerl@gmail.com>
2018-06-29 07:53:48 +0700

    Create join-room page

---
+++ src/views/JoinRoom.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="join-room">
+    <join-room-list></join-room-list>
+  </div>
+</template>
+
+<script>
+import JoinRoomList from '@/components/JoinRoom/JoinRoomList.vue'
+
+export default {
+  name: 'JoinRoom',
+  components: {
+    JoinRoomList
+  }
+}
+</script>
+
+<style lang="scss">
+.join-room {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
